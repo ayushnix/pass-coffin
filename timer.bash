bbd43b19f022560817f3fd6c106f27a09ec4936f
Ayush Agarwal <ayush@fastmail.in>
2021-09-24 01:17:36 +0530

    added interface files for pass-coffin
    the open.bash file is an interface for the `pass open` command which
    will open the encrypted coffin
    
    the close.bash file is an interface for the `pass close` command which
    will close (create) the encrypted coffin
    
    the timer.bash file is an interface for the `pass timer` command which
    shows the status of the systemd user timer meant to close (create) the
    coffin

---
+++ timer.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright (C) 2021 Ayush Agarwal <ayush at fastmail dot in>
+#
+# pass coffin - Password Store Extension (https://www.passwordstore.org)
+# A password store extension that hides data inside a GPG coffin
+# ------------------------------------------------------------------------------
+
+if [[ -x "${PASSWORD_STORE_EXTENSIONS_DIR}/coffin.bash" ]]; then
+  source "${PASSWORD_STORE_EXTENSIONS_DIR}/coffin.bash"
+elif [[ -x "${SYSTEM_EXTENSION_DIR}/coffin.bash" ]]; then
+  source "${SYSTEM_EXTENSION_DIR}/coffin.bash"
+else
+  printf '%s\n' "Unable to load pass coffin. Exiting!" >&2
+  exit 1
+fi
+
+coffin_timer
